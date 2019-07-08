342101334ce46bd994f8eb800641c5e5517afe6e
Hecate-DynMist <CchecateCc@gmail.com>
2019-07-08 17:09:45 -0400

    up

--- uuid_technology_merge.py
+++ uuid_technology_merge.py
@@ -9,7 +9,7 @@ def merge(dfc,dfu):
     df = pd.merge(dfc,dfu,right_on='n.name',left_on='Terminology',how='left')
     df['n.name'] = df['n.name'].fillna('另外')
     df.drop(['ID','Category_old'],1,inplace=True)
-    df.to_csv('uuidmerge.xlsx',index=False)
+    df.to_csv('uuidmerge.csv',index=False)
 
 merge(cat,uuid)
 
